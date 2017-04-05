6db8ba11c02ad1fb1f43bfa901ac0a4f375fb619
Oscar Alberto Cordero Villa <oscar101019@gmail.com>
2017-04-04 19:07:22 -0600

    Inserciones a la parte de Inventarios 04/04/2017

--- ScriptsDB/Insert_DBVironchi.sql
+++ ScriptsDB/Insert_DBVironchi.sql
@@ -56,4 +56,26 @@ select m. id_Mascota, M.Nombre,M.Raza from Cliente C
 inner join Mascota M on(c.id_Cliente=M.id_Cliente)
 WHERE c.Nombre = 'Brian Efren Padilla Simon';
 
+--Inserciones para la parte de inventarios (se debe insertar en el orden en el que estan)
+
+insert into inventario values
+(1,'Medicamentos','Se lleva el control de todos los medicamentos usados en la clinica'),
+(2,'Insumos','Son los productos de uso ccotidiano en la clinica veterinaria'),
+(3,'Productos Tienda','Son los articulos que se venden en la tienda de la clinica');
+
+insert into proveedores(NombreCompañia,NombreContacto) values
+('DogChow','Dylan Parra Dominguez'),
+('whiskas','Emanuel Cardenas Ramos'),
+('Cositas','Doña Cuca');
+
+INSERT INTO unidadmedida VALUES
+(1,'Cajas'),
+(2,'Bolsas');
+
+INSERT INTO producto VALUES
+(1,3,1,2,'Pedrigri','500gr',60.50,10,1),
+(2,3,2,2,'Wiskas','500gr',50.50,5,1),
+(3,2,3,1,'Guantes','2 guantes',5.50,20,1),
+(4,1,3,1,'Morfina','50ml',99.30,20,1);
+
 
